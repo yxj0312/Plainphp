93b4f6c68e4f5527a2f980e4b4b99740c8cebe14
yxj0312 <yanxiajun@hotmail.com>
2020-02-03 12:59:32 +0100

    install di container

--- public/index.php
+++ public/index.php
@@ -2,11 +2,26 @@
 
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
+use Plainphp\HelloWorld;
+use function DI\create;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class)
+]);
+
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(HelloWorld::class);
 
 //echo 'Hello, world!';
 
-$helloworld = new \Plainphp\HelloWorld();
+$helloworld = new HelloWorld();
 $helloworld->announce();
 
 
